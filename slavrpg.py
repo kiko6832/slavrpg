3fa9c289b6b5c3a5ec3c1bbd61a646ad38440d11
kiko6832 <47065036+kiko6832@users.noreply.github.com>
2019-01-27 14:10:17 +0200

    Update slavrpg.py

--- slavrpg.py
+++ slavrpg.py
@@ -11,7 +11,7 @@ Clientdiscord = discord.Client()
 
 @client.event
 async def on_ready():
-    await client.change_presence(game=Game(name='HardBass',type = 2))
+    await client.change_presence(game=Game(name='Solviet Union Anthem',type = 2))
     print("SlavRPG Bot | Python 3.6.5 -> ONLINE")
 
         
