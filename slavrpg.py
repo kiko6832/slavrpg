731508483823f58f036a42c71f4d7caa18c7ac8d
kiko6832 <47065036+kiko6832@users.noreply.github.com>
2019-01-28 20:52:29 +0200

    Add files via upload

--- slavrpg.py
+++ slavrpg.py
@@ -22,21 +22,18 @@ async def status_task():
                 await asyncio.sleep(10)
                 await client.change_presence(game=Game(name='with '+ str(len(client.servers)) +' servers.',type = 0))
                 await asyncio.sleep(10)
-
 @client.event
 async def on_ready():
         client.loop.create_task(status_task())
         print("SlavRPG Bot | Python 3.6.5 -> ONLINE")
         print(f"Logged in as {client.user}")
-    
-@client.event
-async def on_message_delete(message):
-        avtor = message.author
-        content = message.content
-        channel = message.channel
-        await client.send_message(channel, '{} deleted a message. It was: \n**`{}`**'.format(avtor, content))
-        await client.process_commands(message)
-
+#@client.event
+#async def on_message_delete(message):
+#        avtor = message.author
+#        content = message.content
+#        channel = message.channel
+#        await client.send_message(channel, '{} deleted a message. It was: \n**`{}`**'.format(avtor, content))
+#        await client.process_commands(message)
 @client.command()
 async def help():
         embed = discord.Embed(
@@ -45,12 +42,11 @@ async def help():
         colour = discord.Colour.red()
         )
         embed.set_footer(text='SlavRPG Bot - GameKiller02BG - 2019')
-        embed.add_field(name=':game_die:**Game**',value='**`.game`**', inline=False)
-        embed.add_field(name=':gear:**Settings**',value='**`.settings`**', inline=False)
-        embed.add_field(name=':thumbsup:**Vote**',value='**`.vote`**', inline=False)
-        embed.add_field(name=':call_me:**Random**',value='**`.random`**', inline=False)
+        embed.add_field(name=':game_die:**Game**',value='**`.game`**', inline=True)
+        embed.add_field(name=':gear:**Settings**',value='**`.settings`**', inline=True)
+        embed.add_field(name=':thumbsup:**Vote**',value='**`.vote`**', inline=True)
+        embed.add_field(name=':call_me:**Random**',value='**`.random`**', inline=True)
         await client.say(embed=embed)
-        
 @client.command()
 async def game():
         embed = discord.Embed(
@@ -58,9 +54,8 @@ async def game():
         decription = 'Game Help',
         colour = discord.Colour.green())
         embed.set_footer(text='SlavRPG Bot - GameKiller02BG - 2019')
-        embed.add_field(name=':up:**Ranks**',value='**`Shows RankUp costs`**', inline=False)
+        embed.add_field(name=':up:**Ranks**',value='**`Shows RankUp costs`**', inline=True)
         await client.say(embed=embed)
-        
 @client.command()
 async def random():
         embed = discord.Embed(
@@ -68,47 +63,45 @@ async def random():
         decription = 'Random',
         colour = discord.Colour.green())
         embed.set_footer(text='SlavRPG Bot - GameKiller02BG - 2019')
-        embed.add_field(name=':ping_pong:**Ping**',value='**`Replies Pong!`**', inline=False)
-        embed.add_field(name=':middle_finger:**Ligma**',value='**`Surprise!`**', inline=False)
+        embed.add_field(name=':ping_pong:**Ping**',value='**`Replies Pong!`**', inline=True)
+        embed.add_field(name=':middle_finger:**Ligma**',value='**`Surprise!`**', inline=True)
+        embed.add_field(name=':speaking_head:**Clear (amount)**',value='**`Clears the choosen amount of messages.`**', inline=True)
         await client.say(embed=embed)
-
 @client.command()
 async def settings():
         embed = discord.Embed(
         title = 'SlavRPG Bot - Settings',
         decription = 'Settings',
         colour = discord.Colour.blue())
-        embed.set_footer(text='SlavRPG Bot - GameKiller02BG - 2019')
-        embed.add_field(name=':gear:**Soon!**',value='**`Soon!`**', inline=False)
+        embed.set_footer(text='SlavRPG BOT - GameKiller02BG - 2019')
+        embed.add_field(name=':gear:Soon!',value='Soon!', inline=True)
         await client.say(embed=embed)
-
+@client.command()
+async def vote():
+        await client.say('```Vote link: ```')
+@client.command()
+async def ligma():
+        await client.say('```Ligma Balls!```')
+@client.command()
+async def ping():
+        await client.say('```Pong!```')
 @client.command(pass_context=True)
-async def clear(ctx, amount=10):
+async def clear(ctx, amount=100):
         channel = ctx.message.channel
         messages = []
         async for message in client.logs_from(channel, limit=int(amount)+1):
-                messages.append(message)
-                await client.delete_messages(messages)
-                await client.say((amount)+'Messages deleted')
-                await client.say('If you want to delete the confirmation message type `.clearc`')
-
+                        messages.append(message)
+        await client.delete_messages(messages)
+        await client.say('```%s Messages deleted!```'%amount)
 @client.command(pass_context=True)
-async def clearc(ctx):
-        channel = ctx.message.channel
-        messages = []
-        async for message in client.logs_from(channel, limit=int(1)):
-                messages.append(message)
-                await client.delete_messages(messages)
-@client.command()
-async def vote():
-        await client.say('`Vote link: `')
-
-@client.command()
-async def ligma():
-        await client.say('`Ligma Balls! `')
-
-@client.command()
-async def ping():
-        await client.say('`Pong!`')
-
+async def am(ctx, member: discord.Member, *msg):
+    await client.send_message(member, '```Anonimous user sent you a message```')
+    await client.send_message(member, " ".join(msg[0:]))
+    await client.say("Anonimous message has been sent")
+    channel = ctx.message.channel
+    messages = []
+    async for message in client.logs_from(channel, limit = int(2)):
+        messages.append(message)
+    await client.delete_messages(messages)
+    await client.say('%s Messages deleted' %(amount))
 client.run('NTM4NzY0NTM1OTk0MjUzMzQ4.Dy5cFw.nd6cwfdHPNoYUVGpBgI2TdsPakE')
